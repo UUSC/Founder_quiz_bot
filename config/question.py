3e2aa0e933a92f294a19ef1e4dc6d7b8684f4986
usc <aurtemov@mail.ru>
2026-03-27 18:26:03 +0300

    Split by files

---
+++ config/question.py
@@ -0,0 +1,57 @@
+quiz_data = [
+    {
+        'question': 'Что такое Python?',
+        'options': ['Язык программирования', 'Тип данных', 'Музыкальный инструмент', 'Змея на английском'],
+        'correct_option': 0
+    },
+    {
+        'question': 'Какой тип данных используется для хранения целых чисел?',
+        'options': ['int', 'float', 'str', 'natural'],
+        'correct_option': 0
+    },
+    {
+        "question": "Какой тип данных используется для хранения текста в Python?",
+        "options": ["int", "str", "list", "bool"],
+        "correct_option": 1
+    },
+    {
+        "question": "Как создать список в Python?",
+        "options": ["{}", "()", "[]", "<>"],
+        "correct_option": 2
+    },
+    {
+        "question": "Что выведет код print(2 ** 3)?",
+        "options": ["6", "8", "9", "5"],
+        "correct_option": 1
+    },
+    {
+        "question": "Какой цикл используется для перебора элементов последовательности?",
+        "options": ["loop", "while", "for", "repeat"],
+        "correct_option": 2
+    },
+    {
+        "question": "Что делает функция len()?",
+        "options": [
+            "Возвращает тип переменной",
+            "Возвращает длину объекта",
+            "Удаляет элемент",
+            "Сортирует список"
+        ],
+        "correct_option": 1
+    },
+    {
+        "question": "Как объявить функцию в Python?",
+        "options": ["function", "def", "func", "lambda"],
+        "correct_option": 1
+    },
+    {
+        "question": "Что выведет print(type(5))?",
+        "options": ["<class 'int'>", "<class 'str'>", "<class 'float'>", "<class 'bool'>"],
+        "correct_option": 0
+    },
+    {
+        "question": "Какой оператор используется для проверки равенства?",
+        "options": ["=", "==", "!=", ">="],
+        "correct_option": 1
+    }
+]
---
+++ telegram_functions/keybords.py
@@ -0,0 +1,12 @@
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
+from aiogram import types
+def generate_options_keyboard(answer_options, right_answer):
+    builder = InlineKeyboardBuilder()
+    for option in answer_options:
+        builder.add(types.InlineKeyboardButton(
+            text=option,
+            callback_data=answer_options if option == right_answer else "wrong_answer")
+        )
+
+    builder.adjust(1)
+    return builder.as_markup()
